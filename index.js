5a379a573b37d22789da8cf01bec28fc86659408
dingyanhe <yhding9527@hotmail.com>
2022-05-24 13:41:34 +0800

    chore: 更新包名

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const writeFn = new Stream.Writable({
   }
 })
 
-exports.copyFiles2 = (args, config, callback) => {
+exports.copyfiles3 = (args, config, callback) => {
   if (typeof config === 'function') {
     callback = config;
     config = {
