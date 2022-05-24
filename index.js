7beb34ad12391a74f56002394678a26829c4d654
dingyanhe <yhding9527@hotmail.com>
2022-05-24 14:02:24 +0800

    chore: beta

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const writeFn = new Stream.Writable({
   }
 })
 
-exports.copyfiles3 = (args, config, callback) => {
+exports.copyfiles-v3 = (args, config, callback) => {
   if (typeof config === 'function') {
     callback = config;
     config = {
